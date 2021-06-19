4d3ed440c0b1a0585cb7f43ebf7af58a2082921a
Brian Taylor Vann <brian.t.vann@gmail.com>
2021-06-19 01:46:54 -0700

    feat(deno-fy): make lit available in a top-level file

--- v1.3.x/build_lit.sh
+++ v1.3.x/build_lit.sh
@@ -1,7 +1,7 @@
-deno bundle --config ./tsconfig.json ./lit-html/src/lit-html.ts ./lit-html/dist/lit-html.js
-deno bundle --config ./tsconfig.json ./reactive-element/src/reactive-element.ts ./reactive-element/dist/reactive-element.js
+deno bundle --config ./tsconfig.json ./lit-html/src/lit-html.ts ./lit-html/lit-html_bundled.js
+deno bundle --config ./tsconfig.json ./reactive-element/src/reactive-element.ts ./reactive-element/reactive-element_bundled.js
+deno bundle --config ./tsconfig.json ./lit-element/src/index.ts ./lit-element/index_bundled.js
+deno bundle --config ./tsconfig.json ./lit/src/index.ts ./lit/index_bundled.js
+deno bundle --config ./tsconfig.json ./localize/src/lit-localize.ts ./localize/lit-localize_bundled.js
 
-deno bundle --config ./tsconfig.json ./lit-element/src/index.ts ./lit-element/dist/index.js
-deno bundle --config ./tsconfig.json ./lit/src/index.ts ./lit/dist/index.js
-
-deno bundle --config ./tsconfig.json ./localize/src/lit-localize.ts ./localize/dist/lit-localize.js
+deno bundle --config ./tsconfig.json ./lit.ts ./lit_bundled.js
